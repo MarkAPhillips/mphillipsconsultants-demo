0ee5de4fda07adab3393afa44a98a5e3d37a7151
Mark Phillips <work-mpconsultants@outlook.com>
2015-02-07 17:27:48 +0000

    Add less for template based on bootstrap starter template

---
+++ MPhillipsConsultants.Demo.Ui/src/app/common/directives/templatedirectives.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('MPhillipsConsultants.Demo.App.Common')
+        .directive('mpHeaderTemplate', headerTemplate);
+    
+    function headerTemplate () {
+        var directive = { templateUrl: '/src/app/common/views/header.tmpl.html',restrict: 'EA'};
+        return directive;
+    }
+})();
