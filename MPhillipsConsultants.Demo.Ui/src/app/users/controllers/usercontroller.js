c57774b0753ef7da5819cf619f1377763cac2067
Mark Phillips <work-mpconsultants@outlook.com>
2015-02-16 21:48:13 +0000

    Add first part of paging of records

--- MPhillipsConsultants.Demo.Ui/src/app/users/controllers/usercontroller.js
+++ MPhillipsConsultants.Demo.Ui/src/app/users/controllers/usercontroller.js
@@ -5,16 +5,74 @@
         .module('MPhillipsConsultants.Demo.App.User')
         .controller('userController', userController);
 
-    userController.$inject = ['$scope', '$log', '$rootScope','userFactory','_'];
+    userController.$inject = ['$scope', '$log', '$rootScope', 'userFactory', '_'];
 
     function userController($scope, $log, $rootScope, userFactory, _) {
         /* jshint validthis:true */
         var vm = this;
         vm.loaded = false;
         vm.search = search;
-       
+        vm.currentPage = 1;
+        vm.maxSize = 4;
+        vm.pageSize = 10;
+
+        loadUsers(true);
+
+        /* Load user data */
+
+        function loadUsers(emit) {
+            if (emit) {
+                $scope.$emit('load');
+            }
+            userFactory.users().get({ skip: recordsToSkip(), top: vm.pageSize }).$promise.then(function(data) {
+                vm.users = data.value;
+                vm.totalItems = data['@odata.count'];
+                vm.loaded = true;
+            }, function(error) {
+                $log.error(error);
+            }).finally(function() {
+                if (emit) {
+                    $scope.$emit('unload');
+                }
+            });
+        }
+
+        /* Search users */
+
+        function search() {
+            vm.currentPage = 1;
+            $rootScope.message = null;
+            vm.loaded = false;
+            var searchText = vm.searchText;
+            var filterCommand = 'contains(FirstName, \'' + searchText + '\') or ' +
+                'contains(LastName, \'' + searchText + '\')';
+
+            userFactory.users().search({ cmd: filterCommand, skip: recordsToSkip(), top: vm.pageSize }).$promise.then(function(data) {
+                vm.users = data.value;
+                vm.totalItems = data['@odata.count'];
+                vm.loaded = true;
+            }, function(error) {
+                $log.error(error);
+            });
+        }
+
+        /* Determine no of records to skip */
+        function recordsToSkip() {
+            var skip = 0;
+            if (vm.currentPage >= 1) {
+                var page = vm.currentPage;
+                skip = --page * vm.pageSize;
+            }
+            return skip;
+        }
+
+        /* Navigate pages */
+        vm.changePage = function() {
+            loadUsers(false);
+        };
+
         /* Delete user from view */
-        vm.delete = function (index) {
+        vm.delete = function(index) {
             if (confirm('Click OK to delete the user')) {
                 userFactory.users().delete({ key: index }).$promise.then(function(user) {
                     vm.users = _.reject(vm.users, function(item) { return item.Id === index; });
@@ -22,37 +80,5 @@
                 });
             }
         };
-
-        loadUsers();
-         
-        /* Load user data */
-        function loadUsers() {
-            $scope.$emit('load');
-            userFactory.users().get().$promise.then(function(data) {
-                vm.users = data.value;
-                vm.loaded = true;
-            }, function(error) {
-                $log.error(error);
-            }).finally(function() {
-                $scope.$emit('unload');
-            });
-        }
-        
-        /* Search users */
-        function search() {
-            $rootScope.message = null;
-            vm.loaded = false;
-            var searchText = vm.searchText;
-
-            var filterCommand = 'contains(FirstName, \'' + searchText + '\') or ' +
-                'contains(LastName, \'' + searchText + '\')';
-
-            userFactory.users().search({ cmd: filterCommand }).$promise.then(function (data) {
-                vm.users = data.value;
-                vm.loaded = true;
-            }, function (error) {
-                $log.error(error);
-            });
-        }
     }
 })();
